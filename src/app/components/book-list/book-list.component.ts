1d7dc370f5fbbc5b7c98526dda251b626b30bbd3
Kumar Abhinav <abhinav0441@gmail.com>
2018-12-12 10:45:32 +0530

    aaa

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -7,12 +7,17 @@ import { Book } from '../../models/book';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-  book : Book;
+  books : Book[];
 
   constructor() { }
 
   ngOnInit() {
-    this.book = new Book('The Alchemist', 'Paulo Cohelo', 23, 4);
+    this.books = [
+      new Book('The Alchemist', 'Paulo Cohelo', 23, 4),
+      new Book('Five point someone', 'Chetan Bhagat', 12, 2),
+      new Book('The monk who sold his ferrari', 'Robin Sharma', 19, 3),
+      new Book('4 hour work week', 'Tim Ferris', 34, 5)
+    ];
   }
 
 }
